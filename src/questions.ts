cf4218193a14d20cc92577921bfee6dbf8f21e77
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2023-01-16 22:25:21 -0500

    feat: Add more questions

--- src/questions.ts
+++ src/questions.ts
@@ -21,6 +21,26 @@ export const findTense: Question[] = [
     question: 'Nous avons fini nos devoirs hier soir',
     answer: 'le passé composé',
   },
+  {
+    question: "Je visiterai la France l'été prochain",
+    answer: 'le futur simple',
+  },
+  {
+    question: 'Elle écrira une lettre à son ami',
+    answer: 'le futur simple',
+  },
+  {
+    question: "Je visitais souvent ma grand-mère quand j'étais tout petit",
+    answer: "l'imparfait",
+  },
+  {
+    question: 'Elle aimait beaucoup lire des romans',
+    answer: "l'imparfait",
+  },
+  {
+    question: 'Il a acheté un nouveau vélo il y a quelques semaines',
+    answer: 'le passé composé',
+  },
 ].map(question => ({
   ...question,
   category: QuestionCategory.FindTense,
@@ -32,8 +52,8 @@ export const conjugate: Question[] = [
     answer: 'ai mangé',
   },
   {
-    question: 'Tu _____ (perdre, passé composé) ton portefeuille hier matin',
-    answer: 'as perdu',
+    question: 'Il _____ (perdre, passé composé) son portefeuille hier matin',
+    answer: 'a perdu',
   },
   {
     question: 'Il _____ (pleuvoir, passé composé) toute la journée hier',
@@ -43,18 +63,85 @@ export const conjugate: Question[] = [
     question: 'Elle _____ (acheter, passé composé) des fleurs pour sa mère',
     answer: 'a acheté',
   },
+  {
+    question:
+      "J'ai beaucoup aimé les fleurs que tu me _____ (offrir, passé composé)",
+    answer: 'as offertes',
+  },
+  {
+    question: 'Nous _____ (se téléphoner, passé composé) pendant des heures',
+    answer: 'nous sommes téléphoné',
+  },
+  {
+    question: "Elle _____ (s'assoir, passé composé) derrière moi",
+    answer: "s'est assise",
+  },
+  {
+    question: 'Nous _____ (aller, futur simple) à la plage demain après-midi',
+    answer: 'irons',
+  },
+  {
+    question: 'Il _____ (faire, futur simple) beau cette fin de semaine',
+    answer: 'fera',
+  },
+  {
+    question:
+      'Nous _____ (jouer, imparfait) au football dans le parc tous les soirs',
+    answer: 'jouions',
+  },
+  {
+    question: 'Il _____ (pleuvoir, imparfait) souvent en automne',
+    answer: 'pleuvait',
+  },
+  {
+    question:
+      'Nous _____ (prendre, passé composé) le petit déjeuner ensemble ce matin',
+    answer: 'avons pris',
+  },
+  {
+    question: 'Il _____ (neiger, passé composé) toute la nuit hier',
+    answer: 'a neigé',
+  },
+  {
+    question: 'Nous _____ (aller, futur simple) à ce restaurant ce soir',
+    answer: 'allons',
+  },
+  {
+    question: 'Il _____ (être, futur simple) professeur dans deux ans',
+    answer: 'sera',
+  },
+  {
+    question: 'Il _____ (faire, imparfait) toujours chaud en été',
+    answer: 'faisait',
+  },
 ].map(question => ({
   ...question,
   category: QuestionCategory.Conjugate,
 }))
 
-const cdOrCI: Question[] = [
+export const cdOrCI: Question[] = [
   {
     question: "La pomme que j'ai mangée était délicieuse",
     answer: 'CD',
   },
   {
     question: "L'endroit où je suis allé était magnifique",
+    answer: 'CI',
+  },
+  {
+    question: 'Je me suis lavée ce matin',
+    answer: 'CD',
+  },
+  {
+    question: 'Je lui ai parlé la semaine passé',
+    answer: 'CI',
+  },
+  {
+    question: "J'ai visité la Tour Eiffel hier",
+    answer: 'CD',
+  },
+  {
+    question: 'Je suis allé à Lyon pour visiter ma famille',
     answer: 'CI',
   },
 ].map(question => ({
